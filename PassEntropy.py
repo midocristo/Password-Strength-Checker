48b4b3da3dc8fa94abef9abf3da93c3197c7ad59
Made Tiano <mdchristiaan@gmail.com>
2025-11-07 22:56:00 +0800

    Second Commit

--- PassEntropy.py
+++ PassEntropy.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 password = input(str("Input Password: "))
 panjangPassword = len(password)
@@ -8,12 +8,23 @@ hurufBesar = False
 angka = False
 simbol = False
 
-for karakter in range(panjangPassword):
-    if karakter == karakter.lower():
+for karakter in password:
+    if karakter.islower():
         hurufKecil = True
-    elif karakter == karakter.upper():
+    elif karakter.isupper():
         hurufBesar = True
     elif karakter.isdigit():
         angka = True
     else:
         simbol = True
+        
+banyakKarakter = 0
+
+if hurufKecil: banyakKarakter += 26
+if hurufBesar: banyakKarakter += 26
+if angka: banyakKarakter += 10
+if simbol: banyakKarakter += 33
+
+print(banyakKarakter)
+
+kombinasi = float(pow(banyakKarakter, panjangPassword))
