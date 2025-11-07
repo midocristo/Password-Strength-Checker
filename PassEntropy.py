76b211e5ac9f4a16d18bfb6c46444946b2904eff
Made Tiano <mdchristiaan@gmail.com>
2025-11-07 23:10:51 +0800

    Password Checker With Password Entropy

--- PassEntropy.py
+++ PassEntropy.py
@@ -25,6 +25,51 @@ if hurufBesar: banyakKarakter += 26
 if angka: banyakKarakter += 10
 if simbol: banyakKarakter += 33
 
-print(banyakKarakter)
-
 kombinasi = float(pow(banyakKarakter, panjangPassword))
+password_entropy = np.round(np.log2(kombinasi))
+
+if (password_entropy>= 0 and password_entropy <= 35) :
+    print("Your Password is Very Weak!")
+elif (password_entropy >= 36 and password_entropy <= 59) :
+    print("Your Password is Weak!")
+elif (password_entropy >= 60 and password_entropy <= 119) :
+    print("Your Password is Strong!")
+elif (password_entropy >= 120) :
+    print("Your Password is Very Strong!")
+else :
+    print("error!")
+    
+waktu = kombinasi / 10000000  # misal kombinasi = banyakKarakter ** panjangPassword
+
+if waktu < 1:
+    print("It will take less than 1 second to guess!")
+elif waktu < 60:
+    print("It will take {:.1f} seconds to guess!".format(waktu))
+else:
+    waktu /= 60
+    if waktu < 60:
+        print("It will take {:.1f} minutes to guess!".format(waktu))
+    else:
+        waktu /= 60
+        if waktu < 24:
+            print("It will take {:.1f} hours to guess!".format(waktu))
+        else:
+            waktu /= 24
+            if waktu < 31:
+                print("It will take {:.1f} days to guess!".format(waktu))
+            else:
+                waktu /= 31
+                if waktu < 12:
+                    print("It will take {:.1f} months to guess!".format(waktu))
+                else:
+                    waktu /= 12
+                    if waktu < 1000:
+                        print("It will take {:.1f} years to guess!".format(waktu))
+                    else:
+                        waktu /= 1000
+                        if waktu < 1000:
+                            print("It will take {:.1f} thousand years to guess!".format(waktu))
+                        else:
+                            waktu /= 1000
+                            if waktu < 1000:
+                                print("It will take {:.1f} million years to guess!".format(waktu))
